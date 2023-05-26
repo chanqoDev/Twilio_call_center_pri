bb6dc7b9b4d9624cf2f8fcc50d53e2759f566497
Christian E <christianelena92@gmail.com>
2023-05-26 02:03:09 -0500

    init twilio client verify V2, specificy channel type

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -1,27 +1,36 @@
-const {
-  phoneNumber,
-  phoneNumberSid,
-  tokenSid,
-  tokenSecret,
-  accountSid,
-  verify,
-} = require("./config");
-
-console.log(phoneNumber);
+const twilio = require("twilio");
+const config = require("./config");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 class Twilio {
-  client;
-  phoneNumberSid = phoneNumberSid;
-  tokenSid = tokenSid;
-  tokenSecret = tokenSecret;
-  accountSid = accountSid;
-  verify = verify;
+  phoneNumber = config.phoneNumber;
+  phoneNumberSID = config.phoneNumberSID;
+  tokenSid = config.tokenSid;
+  verify = config.verify;
 
-  constructor(accountSid, tokenSecret) {
-    this.client = require("twilio")(this.accountSid, this.tokenSecret);
+  client = require("twilio")(accountSid, authToken);
+
+  constructor() {
+    // this.client = twilio(this.tokenSid, this.authToken, {
+    //   accountSid: this.accountSid,
+    // });
+  }
+  getTwilio() {
+    this.client;
+  }
+
+  async sendVerify(to, channel) {
+    const data = await this.client.verify.v2
+      .services(this.verify)
+      .verifications.create({ to: "to Phone number", channel: "sms" })
+      .then((verification) => console.log(verification.sid));
+    console.log("sendVerify", data);
+    return data;
   }
 }
 
 const instance = new Twilio();
 Object.freeze(instance);
+
 module.exports = instance;
