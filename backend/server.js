a72bf0fad8b2732b4a82b678ab28495449a31058
Christian E <christianelena92@gmail.com>
2023-05-26 01:58:28 -0500

    create socket.io endpoints, spin up server, make a sync dat req to tiwio to verify login use, server connectivity status

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,60 @@
 const express = require("express");
+const twilio = require("./Twilio");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+
+// const socketIo = require("socket.io")(server, {
+//   cors: { origin: "*", methods: ["*"] },
+// });
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:3001",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
+
 const app = express();
-const config = require("./config");
+app.use(cors());
+var server = http.createServer(app);
+// const socket = io(server);
+var socket = socketIo(server);
+
+socket.on("connection", (socket) => {
+  console.log("Socket connected", socket);
+});
+
+socket.on("disconnect", (socket) => {
+  console.log("Socket disconnected", socket);
+});
+
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
 
-// Middleware test: Logging middleware with time stamp
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("Internal Server Error");
-});
-
-//  Twilio SMS Verify Endpoints
 app.get("/test", (req, res) => {
-  res.send("wecome to twilio 101");
+  res.send("<h1>howdy Twilio</h1>");
 });
 
-app.get("/login", (req, res) => {
-  console.log("login in ... ");
+app.get("/login", async (req, res) => {
+  const { to, username, channel } = req.body;
+
+  const data = await twilio.sendVerify(to, channel);
+  res.send(data);
+  console.log(data);
 });
 
-app.get("/verify", (req, res) => {
-  console.log("verifying code");
+app.get("/verfiy", async (req, res) => {
+  console.log("verifing code");
+  const { to, code } = req.body;
+  const data = await twilio.verifyCodeAsync(to, code);
+  res.send(data);
 });
+// console.log(process.env.MOBILE);
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`listening on port: ${PORT}`);
 });
