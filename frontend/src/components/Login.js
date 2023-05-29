2922708607a69ee1415966c93a8810e88dc2dae1
Christian E <christianelena92@gmail.com>
2023-05-29 18:10:51 -0500

    add value, onchange, name to populate onChange

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,41 +1,67 @@
 import React from "react";
-import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Segment,
+  Message,
+  Image,
+} from "semantic-ui-react";
 
-function Login() {
+function Login({ user: { username, mobileNumber }, setUser }) {
+  function populateFields(event, data) {
+    setUser((draft) => {
+      draft[data.name] = data.value;
+    });
+  }
   return (
     <Grid textAlign="center" verticalAlign="middle" style={{ height: "100vh" }}>
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" color="yellow" textAlign="center">
-          Login into your account:{" "}
+        <Header as="h2" color="rgb(238, 90, 36)" textAlign="center">
+          <Image src="https://react.semantic-ui.com/logo.png" /> Log-in into
+          your account:{" "}
         </Header>
-        <Form>
+        <Form size="large">
           <Segment stacked>
             <Form.Input
               fluid
               icon="user"
               iconPosition="left"
               placeholder="User Name"
+              value={username}
+              onChange={(event, data) => {
+                populateFields(event, data);
+              }}
+              name="username"
             />
             <Form.Input
               fluid
-              icon="mobile"
+              icon="mobile alternate"
               iconPosition="left"
               placeholder="Mobile number"
+              value={mobileNumber}
+              onChange={(event, data) => {
+                populateFields(event, data);
+              }}
+              name="mobileNumber"
             />
-
-            <Button.Group fluid>
-              <Button type="submit" color="teal">
-                Sign up
-              </Button>
-              <Button type="submit" color="white">
-                Login
-              </Button>
-              <Button basic type="button">
-                <u>Forgot your password?</u>
-              </Button>
-            </Button.Group>
+            <Button
+              fluid
+              size="large"
+              type="submit"
+              style={{
+                backgroundColor: "rgb(238, 90, 36)",
+                color: "#fff",
+              }}
+            >
+              Login
+            </Button>
           </Segment>
         </Form>
+        <Message>
+          New to Here? <a href="#">Sign Up</a>
+        </Message>
       </Grid.Column>
     </Grid>
   );
